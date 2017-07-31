8373ce10ded84289f25404db5bbb275f3e57b40e
DarkAngel <darkangelba@gmail.com>
2017-07-30 21:28:32 -0300

    Config.GetRelease

--- registry/Configuration.cs
+++ registry/Configuration.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.Win32;
 
 /* ///////////////////////////////////////////////////////////////////////////
@@ -61,6 +63,33 @@ namespace DarkToolz
         }
 
 
+        #endregion
+
+
+        #region :: Release Version
+        public static string GetRelease(TimeZoneInfo target = null)
+        {
+            var assemblyDate = GetAssemblyDateTime(target);
+            var assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+            return $"{assemblyVersion.Major}.{assemblyVersion.Minor}.{assemblyDate:yyyyMMdd}";
+        }
+        public static DateTime GetAssemblyDateTime(TimeZoneInfo target = null)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var filePath = assembly.Location;
+            const int cPeHeaderOffset = 60;
+            const int cLinkerTimestampOffset = 8;
+            var buffer = new byte[2048];
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                stream.Read(buffer, 0, 2048);
+            var offset = BitConverter.ToInt32(buffer, cPeHeaderOffset);
+            var secondsSince1970 = BitConverter.ToInt32(buffer, offset + cLinkerTimestampOffset);
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var linkTimeUtc = epoch.AddSeconds(secondsSince1970);
+            var tz = target ?? TimeZoneInfo.Local;
+            var localTime = TimeZoneInfo.ConvertTimeFromUtc(linkTimeUtc, tz);
+            return localTime;
+        }
         #endregion
     }
 
